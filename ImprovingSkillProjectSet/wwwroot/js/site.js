40289122bd0cb4e4c7aac6d66bbe6fb789afcaaa
Dad S Wonkulah Jr <68390101+dadwonkulahjr@users.noreply.github.com>
2020-12-10 10:34:35 +0000

    Website ready

--- ImprovingSkillProjectSet/wwwroot/js/site.js
+++ ImprovingSkillProjectSet/wwwroot/js/site.js
@@ -1,8 +1,6 @@
-﻿//var para = document.getElementsByTagName("p");
-//var i = 0;
-//while (i <= para.length) {
-//    para[i].innerHTML = "Programming is JavaScript is Fun!";
-//    para[i].style.font.fontsize(20);
-//    para[i].style.fontFamily = "Verdana";
-//    i++;
-//}
+﻿//$(document).ready(function () {
+//    $('.menu-toggle').click(function () {
+//        $('nav').toggleClass('active');
+//    })
+//})
+
